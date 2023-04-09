a93c6ea176dd70e4503838d3fe7264d7f2bcc240
Elshan <beka417@yandex.ru>
2023-04-10 00:47:10 +0300

    Число N

---
+++ chislo N/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Чётные числа от 1 до N:");
+        for (int i = 2; i <= n; i += 2)
+        {
+            Console.Write(i + " ");
+        }
