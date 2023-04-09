a644a99b018949b3b0a154c52a0b81287d963793
Elshan <beka417@yandex.ru>
2023-04-10 00:13:43 +0300

    найти большее или меньшее число

---
+++ bolshee ili menshee/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"{num1} больше, чем {num2}");
+}
+else if (num1 < num2)
+{
+    System.Console.WriteLine($"{num1} меньше, чем {num2}");
+}
+else
+{
+    System.Console.WriteLine($"{num1} равно {num2}");
+}
