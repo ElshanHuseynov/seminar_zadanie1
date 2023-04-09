093af6f4a42a0e2473eebead7f688ffea9f1f6d0
Elshan <beka417@yandex.ru>
2023-04-10 01:30:47 +0300

    Дни недели

---
+++ den_nedeli/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите день недели (1-7:) ");
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+    System.Console.WriteLine("Это выходной день ");
+}
+else
+{
+    System.Console.WriteLine("Это рабочий день ");
+}
