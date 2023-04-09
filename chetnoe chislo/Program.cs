4b3a53db37affa38845c3479f6c4d1bf90bfbdf6
Elshan <beka417@yandex.ru>
2023-04-10 00:41:25 +0300

    четное число

---
+++ chetnoe chislo/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ведите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    System.Console.WriteLine($" Число {num} является четным! ");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} является нечетным! ");
+}
