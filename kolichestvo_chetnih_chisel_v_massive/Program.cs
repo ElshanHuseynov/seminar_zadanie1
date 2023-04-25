3808f0069c5460861f4e59f2c1524b57dabd338c
Elshan <beka417@yandex.ru>
2023-04-25 12:43:34 +0400

    Количество четных чисел в массиве

---
+++ kolichestvo_chetnih_chisel_v_massive/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] arr = new int[10];
+Random random = new Random();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = random.Next(100, 1000);
+}
+
+Console.WriteLine("Элементы массива:");
+
+int count = 0;
+
+foreach (int element in arr)
+{
+    Console.WriteLine(element);
+    if (element % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чётных чисел в массиве: {count}");
+    
