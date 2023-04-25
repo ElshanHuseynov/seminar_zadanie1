8f5840f1e6ab86f77a532d86c728279f235147e5
Elshan <beka417@yandex.ru>
2023-04-25 12:54:38 +0400

    Находим разницу между максимальным и минимальным элементами массива

---
+++ raznica_mejdu_maksimalnim_i_minimalnim_elementami_massiva/Program.cs
@@ -0,0 +1,24 @@
+﻿double[] arr = { 1.5, 3.7, 2.1, 4.8, 0.9, 2.4, 5.2, 3.0 };
+
+double max = arr[0];
+double min = arr[0];
+
+     
+for (int i = 1; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    else if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+}
+
+double diff = max - min;
+
+Console.WriteLine($"Максимальный элемент массива: {max}");
+Console.WriteLine($"Минимальный элемент массива: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементами: {diff}");
+    
